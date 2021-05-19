4c027f4911341594a7265f0c4a9c4138e5498ad3
Le Thi Tuong Vi <5951071119@st.utc2.edu.vn>
2021-05-19 12:20:18 +0700

    Vi da dang

---
+++ WebBanHang/WebBanHang/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers
+{
+    public class CustomerController : Controller
+    {
+        QuanLyBanHangOnlEntities db = new QuanLyBanHangOnlEntities();
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanHang/WebBanHang/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult OurCoffee()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult Add_Account()
+        {
+            return View();
+        }
+        public ActionResult ForGotPassWord()
+        {
+            return View();
+        }
+        public ActionResult PassWord()
+        {
+            return View();
+        }
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        public ActionResult Order()
+        {
+            return View();
+        }
+    }
+
+}
