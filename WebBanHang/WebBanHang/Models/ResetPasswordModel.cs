aa4896252c0b4863979fdd29ccd14b8e919d122b
Le Thi Tuong Vi <5951071119@st.utc2.edu.vn>
2021-06-12 16:41:37 +0700

    Vi da dang

--- WebBanHang/WebBanHang/Models/ResetPasswordModel.cs
+++ WebBanHang/WebBanHang/Models/ResetPasswordModel.cs
@@ -7,11 +7,12 @@ using System.Web;
 namespace WebBanHang.Models
 {
     public class ResetPasswordModel
-    {   
+    {   [Display(Name ="Mật khẩu mới")]
         [Required(ErrorMessage ="Vui lòng nhập mật khẩu mới")]
         [DataType(DataType.Password)]
         public string NewPassWord { get; set; }
 
+        [Display(Name ="Nhập lại mật khẩu mới")]
         [DataType(DataType.Password)]
         [Compare("NewPassWord", ErrorMessage ="Mật khẩu mới và nhập lại mật khẩu mới không giống nhau!")]
         public string ConfirPassWord { get; set; }
